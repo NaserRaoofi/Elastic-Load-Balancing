490a62791bd3b42b72456df91448681a0cedbf51
NaserRaoofi <sirwan.raofi1370@gmail.com>
2025-03-04 14:34:37 +0000

    the first deployment of the project

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "lab"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances"
+  type        = number
+  default     = 2
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0c7217cdde317cfec"  # Amazon Linux 2023 AMI in us-east-1
+} 
